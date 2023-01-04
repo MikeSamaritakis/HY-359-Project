8951259862cca840562342ad4a02c0d147267fd5
MikeSamaritakis <mikesamaritakis@gmail.com>
2023-01-05 01:25:56 +0200

    created the 2 new servlets AdminGuest.java and StudentLibrarian.java

---
+++ software/team31/src/main/java/StudentLibrarian.java
@@ -0,0 +1,22 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "StudentLibrarian", value = "/StudentLibrarian")
+public class StudentLibrarian extends HttpServlet {
+    private static final long serialVersionUID = 1L; //https://www.codejava.net/coding/java-servlet-and-jsp-hello-world-tutorial-with-eclipse-maven-and-apache-tomcat
+
+    public StudentLibrarian(){
+        super();
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
