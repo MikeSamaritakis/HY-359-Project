c0c536a77b9de4a0ce46f36926568e68ce497266
Michail Samaritakis <mikesamaritakis@gmail.com>
2023-01-06 01:21:40 +0200

    updated all servlet impotrs

--- software/team31/src/main/java/Servlets/AdminGuest.java
+++ software/team31/src/main/java/Servlets/AdminGuest.java
@@ -3,7 +3,35 @@ package Servlets;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+
+import database.tables.EditStudentsTable;
+import database.tables.EditBooksInLibraryTable;
+import database.tables.EditBooksTable;
+import database.tables.EditAdminMessageTable;
+import database.tables.EditBorrowingTable;
+import database.tables.EditLibrarianTable;
+import database.tables.EditReviewsTable;
+import database.tables.GeneralQueries;
+
+import mainClasses.AdminMessage;
+import mainClasses.Book;
+import mainClasses.BookInLibrary;
+import mainClasses.Borrowing;
+import mainClasses.JSON_Converter;
+import mainClasses.Librarian;
+import mainClasses.Review;
+import mainClasses.Student;
+import mainClasses.User;
+
+import java.util.logging.*;
+import com.google.gson.JsonObject;
+import java.sql.*;
 
 @WebServlet(name = "Servlets.AdminGuest", value = "/Servlets.AdminGuest")
 public class AdminGuest extends HttpServlet {
@@ -17,6 +45,27 @@ public class AdminGuest extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     //adminlogin
     //use the dispatcher to redirect to welcome page if login is successful
+        PrintWriter writer = response.getWriter();
+
+        String username = request.getParameter("USERNAME");
+        String password = request.getParameter("PASSWORD");
+
+        writer.println(username);
+        writer.println(password);
+
+//        Student p = new Student();
+//        try {
+//            p = EditStudentsTable.databaseToStudent(username,password);
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        } catch (ClassNotFoundException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        String un = p.getUsername();
+//        writer.println(un);
+
+        writer.close();
     }
 
     @Override
--- software/team31/src/main/java/Servlets/LoginWelcomeLibrarian.java
+++ software/team31/src/main/java/Servlets/LoginWelcomeLibrarian.java
@@ -3,7 +3,35 @@ package Servlets;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+
+import database.tables.EditStudentsTable;
+import database.tables.EditBooksInLibraryTable;
+import database.tables.EditBooksTable;
+import database.tables.EditAdminMessageTable;
+import database.tables.EditBorrowingTable;
+import database.tables.EditLibrarianTable;
+import database.tables.EditReviewsTable;
+import database.tables.GeneralQueries;
+
+import mainClasses.AdminMessage;
+import mainClasses.Book;
+import mainClasses.BookInLibrary;
+import mainClasses.Borrowing;
+import mainClasses.JSON_Converter;
+import mainClasses.Librarian;
+import mainClasses.Review;
+import mainClasses.Student;
+import mainClasses.User;
+
+import java.util.logging.*;
+import com.google.gson.JsonObject;
+import java.sql.*;
 
 @WebServlet(name = "Servlets.LoginWelcomeLibrarian", value = "/Servlets.LoginWelcomeLibrarian")
 public class LoginWelcomeLibrarian extends HttpServlet {
