2b74790fffa84bf179ac6509cab298cd92b1b49b
Michail Samaritakis <mikesamaritakis@gmail.com>
2023-01-29 20:18:53 +0200

    rediorect

--- software/team31/src/main/java/Servlets/StudentLibrarian.java
+++ software/team31/src/main/java/Servlets/StudentLibrarian.java
@@ -66,12 +66,10 @@ public class StudentLibrarian extends HttpServlet {
         }
         // If the log-in fails then an exception is thrown meaning that the user will not see the
         // page created as a welcome user, thus he/she will not have access to any sensitive information.
-         //String updateemail = p.getEmail();
-         String updatefirstname = p.getFirstname();
-         String updatelastname =  p.getLastname();
-        RequestDispatcher rd= request.getRequestDispatcher("/Student.html");
-        rd.forward(request,response);
 
+        String page="index.jsp";
+        RequestDispatcher dd =request.getRequestDispatcher(page);
+        dd.forward(request,response);
     }
 
     @Override
@@ -100,7 +98,6 @@ String page = "";
            page="index.jsp";
            RequestDispatcher dd =request.getRequestDispatcher(page);
            dd.forward(request,response);
-           //πατερας σου
         }
 
 
