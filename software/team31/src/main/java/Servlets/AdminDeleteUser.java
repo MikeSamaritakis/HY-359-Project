187bc73e2f011ed70aa295dfec67bc5f92999566
Michail Samaritakis <mikesamaritakis@gmail.com>
2023-01-29 22:08:10 +0200

    student

--- software/team31/src/main/java/Servlets/AdminDeleteUser.java
+++ software/team31/src/main/java/Servlets/AdminDeleteUser.java
@@ -21,28 +21,18 @@ public class AdminDeleteUser extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = request.getParameter("userToBeDeleted");
-        String type = request.getParameter("TypeDelete");
-        PrintWriter out = response.getWriter();
-        out.println(type);
-        out.println(username);
-        if (type == "1"){
+
             try {
                 EditLibrarianTable.deleteLibrarian(username);
             } catch (ClassNotFoundException e) {
                 throw new RuntimeException(e);
             }
-        }
-        else if (type == "2"){
+
             try {
                 EditStudentsTable.deleteStudent(username);
             } catch (ClassNotFoundException e) {
                 throw new RuntimeException(e);
             }
-        }
-        else if (type == "0"){
-            System.out.println("No user type selected.");
-            System.err.println("Error occurred, not type selected by admin.");
-        }
     }
 
     @Override
